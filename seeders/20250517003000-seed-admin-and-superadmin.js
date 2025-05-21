349a56943cc5cf6e9030ec3ea4b38c67e1738b04
Medi G Fulgence <lycorisblue99@gmail.com>
2025-05-21 13:09:51 +0000

    fix: Remplacement des identifiants générés par crypto par des identifiants statiques pour les admins

--- seeders/20250517003000-seed-admin-and-superadmin.js
+++ seeders/20250517003000-seed-admin-and-superadmin.js
@@ -8,7 +8,7 @@ module.exports = {
 
     await queryInterface.bulkInsert('admins', [
       {
-        id: crypto.randomUUID(),
+        id: "61a639aa-8e7e-4e64-a0b7-59294a538d9a",
         name: 'Admin Principal',
         email: 'admin@example.com',
         password: hashedPassword,
@@ -17,7 +17,7 @@ module.exports = {
         updated_at: new Date()
       },
       {
-        id: crypto.randomUUID(),
+        id: "8ec85df4-b21f-41fa-8b6a-6e49b293f3bd",
         name: 'Super Admin',
         email: 'superadmin@example.com',
         password: hashedSuperPassword,
